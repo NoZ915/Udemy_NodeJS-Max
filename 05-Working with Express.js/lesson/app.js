60e824f51245aead2101cffd9fbe93bb36b23214
NoZ915 <tina0915tw0915@gmail.com>
2023-12-07 11:45:50 +0800

    finished: ~L70

--- 05-Working with Express.js/lesson/app.js
+++ 05-Working with Express.js/lesson/app.js
@@ -4,24 +4,14 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.use("/", (req,res,next) => {
-    next();
-})
-app.use("/add-product", (req, res, next) => {
-    res.send(`
-    <form action="/product" method="POST">
-        <input type="text" name="title">
-        <button type="submit">Add</button>
-    </form>
-    `);
-})
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-})
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 app.use("/", (req, res, next) => {
-    res.send(`<h1>Hello from Express!!!!</h1>`);
+    res.status(404).send(`<h1>Page not Found!</h1>`);
 })
 
 app.listen(3000);
