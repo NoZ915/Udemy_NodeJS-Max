e71e86225ac56353015e6966ce6e7ee6396276dd
NoZ915 <tina0915tw0915@gmail.com>
2023-12-14 19:48:25 +0800

    finished: ~L110

---
+++ 06-Templating Engines/controllers/admin.js
@@ -0,0 +1,27 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product"
+  })
+}
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, price, description); //送出表單 > 發送POST request > 建立Product實例
+  product.save(); // save()會把this（也就是自己，product）推進products陣列中
+  res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products", path: "/admin/porducts"
+    });
+  });
+}
--- 06-Templating Engines/models/product.js
+++ 06-Templating Engines/models/product.js
@@ -1,16 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = class Product{
-    constructor(title){
-        this.t = title;
+module.exports = class Product {
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
-    save(){
+    save() {
         const p = path.join(path.dirname(require.main.filename), "data", "product.json");
         fs.readFile(p, (err, fileContent) => {
             let products = [];
-            if(!err){
+            if (!err) {
                 products = JSON.parse(fileContent);
             }
             products.push(this);
@@ -20,10 +23,10 @@ module.exports = class Product{
         })
     }
 
-    static fetchAll(cb){
+    static fetchAll(cb) {
         const p = path.join(path.dirname(require.main.filename), "data", "product.json");
         fs.readFile(p, (err, fileContent) => {
-            if(err){ //有err代表讀取不到p，可能是還未建立product.json
+            if (err) { //有err代表讀取不到p，可能是還未建立product.json
                 cb([]);
             }
             cb(JSON.parse(fileContent));
