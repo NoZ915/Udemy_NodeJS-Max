efd3b89d471020f89568ff0d78a0500ee1b0fd32
NoZ915 <tina0915tw0915@gmail.com>
2023-12-30 15:58:22 +0800

    finished: ~L196

--- 06-Templating Engines/app.js
+++ 06-Templating Engines/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 const mongoConnect = require('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -18,15 +20,14 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  next();
+  User.findById("658fa9ce14954e00073625c8")
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.use('/admin', adminRoutes);
--- 06-Templating Engines/models/user.js
+++ 06-Templating Engines/models/user.js
@@ -1,16 +1,46 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User{
+  constructor(username, email, cart, id){
+    this.name = username;
+    this.email = email;
+    this.cart = cart ? cart : {};
+    this._id = id;
+    this.cart.items = cart ? cart.items : [];
+  }
+
+  save(){
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+  }
+
+  addToCart(product){
+    const updatedCart = {items:[{...product, quantity: 1}]};
+    const db = getDb(updatedCart);
+    return db
+      .collection("users")
+      .updateOne({_id: new ObjectId(this._id)},{$set: {cart: updatedCart}})
+
+  }
+
+  static findById(userId){
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({_id: new ObjectId(userId)})
+      .then(user => {
+        console.log(`I find it: ${user._id}`)
+        return user;
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
+}
 
 module.exports = User;
