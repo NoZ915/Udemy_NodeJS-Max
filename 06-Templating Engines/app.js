a8f35684a14f6654a2a3d1a4118e35c8d716303a
NoZ915 <tina0915tw0915@gmail.com>
2024-01-15 01:22:31 +0800

    finished: ~L233

--- 06-Templating Engines/app.js
+++ 06-Templating Engines/app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use((req, res, next) => {
   User.findById("65a3562f7ab07fbe0099a42e")
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 const errorController = require("./controllers/error");
 app.use(errorController.get404);
@@ -37,11 +39,11 @@ app.use(errorController.get404);
 mongoose.connect('mongodb+srv://noz915:noz915@cluster0.u80fykc.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
     User.findOne().then(user => {
-      if(!user){
+      if (!user) {
         const user = new User({
           name: "Max",
           email: "abc@gmail.com",
-          cart: {items: []}
+          cart: { items: [] }
         })
         user.save();
       }
